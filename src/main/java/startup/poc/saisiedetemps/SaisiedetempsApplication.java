9093769387886cc3df886879829a6b64ef2ca323
medrare.oumaima <oumaima.medrare@gmail.com>
2024-01-20 12:59:47 +0100

    Initial commit

---
+++ src/main/java/startup/poc/saisiedetemps/SaisiedetempsApplication.java
@@ -0,0 +1,13 @@
+package startup.poc.saisiedetemps;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SaisiedetempsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SaisiedetempsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/startup/poc/saisiedetemps/SaisiedetempsApplicationTests.java
@@ -0,0 +1,13 @@
+package startup.poc.saisiedetemps;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SaisiedetempsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
